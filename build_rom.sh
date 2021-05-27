4582a21a7f0ef909cd0c05e656694fdaac6fa98b
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-27 13:01:11 +0530

    Wave OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/Wave-Project/manifest -b r --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch wave_RMX1941-userdebug
+brunch RMX1941
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
