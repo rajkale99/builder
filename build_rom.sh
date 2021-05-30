9242b1b0330f35c95cf38cc709e400ed61048f38
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-30 20:08:32 +0530

    Added patches

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,12 @@ repo init -u git://github.com/AospExtended/manifest.git -b 11.x --depth=1 --no-r
 git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# patches
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
 # build rom
 source build/envsetup.sh
 lunch aosp_RMX1941-userdebug 
