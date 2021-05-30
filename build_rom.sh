c4e3cc8411d4e2ec3fd9fc4e29e5934eedc6f4b2
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-30 17:50:45 +0530

    AEX

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/Wave-Project/manifest -b r --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
+repo init -u git://github.com/AospExtended/manifest.git -b 11.x --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch wave_RMX1941-userdebug 
-brunch RMX1941 
+lunch aosp_RMX1941-userdebug 
+m aex
 
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
