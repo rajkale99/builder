5fa9e85fa868fe35f4792c6573931ffbfc8c2172
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-31 23:09:00 +0530

    start before sleeping

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b Lineage-FE .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 lunch lineage_mido-userdebug
