9fa7dea097cc24f16b398ed2d75aac4a9036b8b6
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-01 05:35:48 +1000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init -u https://github.com/PotatoProject/manifest -b dumaloo-release --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b POSP .repo/local_manifests
+repo init -u repo init -u https://github.com/descendant-xi/manifests.git -b eleven-staging --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Yasundram/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # patches
@@ -11,8 +11,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch potato_RMX1941-userdebug 
-brunch RMX1941 
+lunch descendant_RMX1941-userdebug
+mka descendant
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
