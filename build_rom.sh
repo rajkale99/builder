2d3a9ed974e220e7fe33db9ac7efd04ad144f54f
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-02 09:33:23 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u repo init -u https://github.com/descendant-xi/manifests.git -b eleven-staging --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Yasundram/local_manifest --depth=1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # patches
 #cd external/selinux
@@ -14,6 +14,5 @@ source build/envsetup.sh
 lunch descendant_RMX1941-userdebug
 mka descendant
 
-# upload rom
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
- 
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
