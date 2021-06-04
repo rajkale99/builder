3c494effc7f8da296fe74cd3e81dcac1bf6baa1b
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-04 20:56:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,4 +8,4 @@ lunch lineage_mido-user
 mka bacon
 
 # Upload build
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
