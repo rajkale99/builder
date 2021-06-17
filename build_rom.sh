72aea501b7d7eaaf54afbca8ec3f481caa8f72aa
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-17 09:19:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/Yasundram/local_manifest --depth=1 -b desc' .repo/local_manifests
+git clone https://github.com/Yasundram/local_manifest --depth=1 -b desc .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
