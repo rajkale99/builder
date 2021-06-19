6d4862dc2b2952cb8089d48a6c083daec13b64a5
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-19 18:29:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/HyconOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b derp .repo/local_manifests
+git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Lavender .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
