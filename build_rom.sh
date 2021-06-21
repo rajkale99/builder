5212aa778ba1261a5e0e334b242be5d65d8b090a
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-21 18:18:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
  source build/envsetup.sh
 lunch cygnus_lavender-userdebug
+export TZ=Asia/Dhaka #put before last build command (Time zone)
 make cygnus -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
