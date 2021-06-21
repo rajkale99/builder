4de7d7b86b051384e50309810905b35c69cdcfad
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-21 13:20:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/NezukoOS/manifest -b eleven .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/mukulsharma06175/frostmanifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
