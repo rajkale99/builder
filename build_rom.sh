998c1f0390150509fa8b29dcc13ff77e70643373
Rahif M <rahifmanjatha372@gmail.com>
2021-06-23 20:44:47 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -10,9 +10,4 @@ export TZ=Asia/Kolkata
 make -j24 otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-up(){
-	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-	# 14 days, 10 GB limit
-}
-up out/target/product/mido/hentai_mido-Rika-ota*.zip
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
