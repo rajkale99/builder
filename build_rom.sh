21f5c44454943cfade51fb738a7865d1daf8a71e
Rahif M <rahifmanjatha372@gmail.com>
2021-06-23 18:54:57 +0530

    Move back to local m and fix upload?

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_manifest/ -b Rika -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/hos-rika .repo/local_manifests
+git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/hos-rika-local_m .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch hentai_mido-userdebug
 export TZ=Asia/Kolkata
-#make -j24 otapackage
+make -j24 otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+up out/target/product/mido/hentai_mido-Rika-ota*.zip
