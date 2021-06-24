4be1a4ee205e1ac1e3416a94606bfe5b5a109fc0
Rahif M <rahifmanjatha372@gmail.com>
2021-06-24 18:36:44 +0530

    Fix &&

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,8 @@ lunch hentai_mido-userdebug
 export TZ=Asia/Kolkata
 make -j24 otapackage
 
-cd out/tar*/pro*/mido && find -name ipacm*
+cd out/tar*/pro*/mido 
+find -name ipacm*
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
