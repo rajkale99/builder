d9d37882335d1f2aaee0533cc04512fc66981e77
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-06-26 14:16:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest.git -b fire -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b spark .repo/local_manifests
+git clone https://github.com/Prashant-1695/frostmanifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
