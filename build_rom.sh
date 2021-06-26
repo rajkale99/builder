f997263a0354cd861e4e9ee7eee9e2cca513ea61
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-26 10:44:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch havoc_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command (Time zone)
-export USE_GAPPS=true
+export WITH_GAPPS=true 
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
