195f36d03f445d764b2078f65889e4a4f60b4a20
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-07-01 16:30:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_RMX1801-userdebug
+lunch corvus_RMX1801-eng
 export TZ=Asia/Kolkata 
 export SELINUX_IGNORE_NEVERALLOWS=true
 make corvus
