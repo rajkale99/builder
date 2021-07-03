7474d6aad606893c1c46be69d1579c62bd931f97
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-04 03:56:32 +0700

    try experiment

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RevengeOS/android_manifest.git -b r11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/D4rkKnight21/local_manifest.git --depth 1 -b ros .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CAF-Extended/manifest -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/D4rkKnight21/local_manifest --depth 1 -b cafex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
+lunch cafex_platina-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch revengeos_platina-userdebug
-export SKIP_API_CHECK=true
-export SKIP_ABI_CHECK=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
-make bacon
+m cafex -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
