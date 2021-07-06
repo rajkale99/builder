99970f37add242a27d09a970165a0a11960ebc51
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-07-06 18:38:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifes
 git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch komodo_lavender-userdebug
 export TZ=Asia/kolkata #put before last build command (Time zone)
