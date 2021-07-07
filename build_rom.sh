607bb74c47accab2d0af1447925cae746b5bb16d
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-07-07 18:23:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,12 +2,11 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-       
+        
 # build rom     
 . build/envsetup.sh
 lunch komodo_lavender-userdebug
 export TZ=Asia/kolkata #put before last build command (Time zone)
-export KOMODO_GAPPS_TYPE := GAPPS
 masak komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
